f02e01a86892ae83e0dc2cf629cfd843980b454f
Kuredema7 <kuredema7@gmail.com>
2021-11-20 19:21:37 +0300

    removed some useless lines

--- Algorithms/solve_me_first.py
+++ Algorithms/solve_me_first.py
@@ -3,11 +3,8 @@ TASK:-
 Complete the function solveMeFirst to compute the sum of two integers.
 
 '''
-
-
 def solveMeFirst(a,b):
     return (a + b)
-
 
 num1 = int(input('Enter 1st number: '))
 num2 = int(input('Enter 2nd number: '))
