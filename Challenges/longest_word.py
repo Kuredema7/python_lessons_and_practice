f323c89f840fc89af72e1dfbbc60652d459a54a8
unknown <kuredema7@gmial.com>
2021-11-26 21:56:53 +0300

    removed the print function inside the for loop

--- Challenges/longest_word.py
+++ Challenges/longest_word.py
@@ -19,6 +19,5 @@ longest_word = ''
 for i in range(len(words) - 1):
     if len(words[i]) > len(longest_word):
         longest_word = words[i]
-        print(f'Longest word: {longest_word}\tWord on now: {words[i]}')
 
 print('Longest word:', longest_word)
